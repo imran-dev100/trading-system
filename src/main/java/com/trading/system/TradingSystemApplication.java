5f84da193e5b7d44275baccbc384aadf742a4b2a
imran-dev100 <iagro7189@gmail.com>
2023-10-19 16:20:43 +0530

    Renaming main class

--- src/main/java/com/trading/system/TradingSystemApplication.java
+++ src/main/java/com/trading/system/TradingSystemApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemApplication {
+public class TradingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SystemApplication.class, args);
+		SpringApplication.run(TradingSystemApplication.class, args);
 	}
 }
