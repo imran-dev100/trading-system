feb73fef14a62096a80f0cd83eba9159ceebff03
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-03-25 12:14:13 +0530

    Update TradingSystemApplication.java

--- src/main/java/com/trading/system/TradingSystemApplication.java
+++ src/main/java/com/trading/system/TradingSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TradingSystemApplication {
-
 	public static void main(String[] args) {	
 		SpringApplication.run(TradingSystemApplication.class, args);
 	}
